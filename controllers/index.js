2278c36da239db3d5f6b57b3b7551fdea8c5e79a
Justin Sanchez <justinlsanchez12@gmail.com>
2023-06-26 23:06:15 -0700

    Updated the routes file by adding in imageRoutes.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
+const imageRoutes = require('./imageRoutes');
 
 router.use('/', homeRoutes);
+router.use('/images', imageRoutes);
 
 module.exports = router;
