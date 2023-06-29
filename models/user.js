1003b925f194f6f323c772daba2080c6849a2610
Justin Sanchez <justinlsanchez12@gmail.com>
2023-06-28 20:34:27 -0700

    Added model for user choice

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class User extends Model {}
+
+//This model is when the user will searches/selects an item that they would want to see
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
