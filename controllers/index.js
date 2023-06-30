6c6046e4bc175b6dda1502956dfd032d8353e282
Justin Sanchez <justinlsanchez12@gmail.com>
2023-06-29 19:42:16 -0700

    updating/deleting route files

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const imageRoutes = require('./imageRoutes');
+const craftingRecipeRoutes = require('./craftingRecipeRoutes');
+
 
 router.use('/', homeRoutes);
 router.use('/images', imageRoutes);
+router.use('/crafting-recipes', craftingRecipeRoutes);
+
 
 module.exports = router;
