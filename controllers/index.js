b8f7a8bcc59bad4f5a7c375487279d9fef128455
Justin Sanchez <justinlsanchez12@gmail.com>
2023-07-03 20:00:57 -0700

    slight updates

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,13 @@ const homeRoutes = require('./homeRoutes');
 const imageRoutes = require('./imageRoutes');
 const apiRoutes = require('./api');
 const craftingRecipeRoutes = require('./craftingRecipeRoutes');
+const itemImageRoutes = require('./itemImageRoutes');
 
 router.use('/', homeRoutes);
 router.use('/images',imageRoutes);
 router.use('/crafting-recipes', craftingRecipeRoutes);
 router.use('/api', apiRoutes);
+router.use('/search', itemImageRoutes);
 
 
 module.exports = router;
