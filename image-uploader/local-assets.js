90947c682ea354af08a710481ee5a6ab799e5111
Justin Sanchez <justinlsanchez12@gmail.com>
2023-07-03 20:01:41 -0700

    uploaded images to cloudinary

--- image-uploader/local-assets.js
+++ image-uploader/local-assets.js
@@ -8,11 +8,77 @@ cloudinary.config({
 });
 
 const images = [
-  './image-uploader/images/axes-crafting.gif',
+ /*
   './image-uploader/images/wood-stairs-crafting.gif',
   './image-uploader/images/tnt-crafting.png',
   './image-uploader/images/hay-bale-crafting.png',
-  './image-uploader/images/carpet-crafting.gif'
+  './image-uploader/images/carpet-crafting.gif',
+
+  //Basic Recipes
+  './image-uploader/images/wood-planks-crafting.gif',
+  './image-uploader/images/chest-crafting.png',
+  './image-uploader/images/crafting-table-crafting.png',
+  './image-uploader/images/furnace-crafting.png',
+  './image-uploader/images/torches-crafting.png',
+  
+  //Tool Recipe
+  //'./image-uploader/images/axes-crafting.gif',
+  './image-uploader/images/bucket-crafting.png',
+  './image-uploader/images/carrot-on-a-stick-crafting.png',
+  './image-uploader/images/clock-crafting.png',
+  './image-uploader/images/compass-crafting.png',
+  './image-uploader/images/fishing-rod-crafting.png',
+  './image-uploader/images/flint-and-steel-crafting.png',
+  './image-uploader/images/hoes-crafting.gif',
+  './image-uploader/images/map-crafting.png',
+  './image-uploader/images/pickaxes-crafting.gif',
+  './image-uploader/images/shears-crafting.png',
+  './image-uploader/images/shovels-crafting.gif',
+
+  //Weapon Recipe
+  './image-uploader/images/bow-crafting.png',
+  './image-uploader/images/arrow-crafting.png',
+  './image-uploader/images/damaged-shield-crafting.png',
+  './image-uploader/images/patterned-shield-crafting.png',
+  './image-uploader/images/shield-crafting.gif',
+  './image-uploader/images/spectral-arrow-crafting.png',
+  './image-uploader/images/swords-crafting.gif',
+  './image-uploader/images/tipped-arrow-crafting.png',
+ 
+
+  //Armor Recipe
+  './image-uploader/images/boots-crafting.gif',
+  './image-uploader/images/chestplates-crafting.gif',
+  './image-uploader/images/helmets-crafting.gif',
+  './image-uploader/images/leggins-crafting.gif',
+
+  //Manufactured Recipe
+  './image-uploader/images/andesite-crafting.png',
+  './image-uploader/images/anvil-crafting.png',
+  './image-uploader/images/beacon-crafting.png',
+  './image-uploader/images/coal-block-crafting.png',
+  './image-uploader/images/quartz-block-crafting.png',
+  './image-uploader/images/redstone-block-crafting.png',
+  './image-uploader/images/bone-block-crafting.png',
+  './image-uploader/images/bookshelf-crafting.png',
+  './image-uploader/images/brick-block-crafting.png',
+  './image-uploader/images/chiseled-quartz-block-crafting.png',
+  './image-uploader/images/chiseled-stone-brick-crafting.png',
+  './image-uploader/images/chiseled-stone-brick-crafting.png',
+  './image-uploader/images/clay-block-crafting.png',
+  './image-uploader/images/coarse-dirt-crafting.png',
+  './image-uploader/images/dark-prismarine-crafting.png',
+  './image-uploader/images/diorite-crafting.png',
+  './image-uploader/images/end-stone-bricks-crafting.png',
+  './image-uploader/images/glowstone-crafting.png',
+  './image-uploader/images/granite-crafting.png',
+  './image-uploader/images/hay-bale-crafting.png',
+  './image-uploader/images/jack-o-lantern-crafting.png',
+  './image-uploader/images/magma-block-crafting.png',
+  './image-uploader/images/moss-stone-crafting.png',
+  './image-uploader/images/mossy-stone-brick-crafting.png',
+  */
+
 ];
 
 (async function run() {
