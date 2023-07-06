f60a3fe3468c46cce52d0f660b9bcf08ae0c4d93
Michael O'Sullivan <michaelhallosullivan@gmail.com>
2023-07-06 09:15:13 -0700

    got comments to display under each respective image

--- controllers/api/imageRoutes.js
+++ controllers/api/imageRoutes.js
@@ -4,6 +4,7 @@ const cloudinary = require('cloudinary').v2;
 const CraftingRecipe = require('../../models/craftingRecipe');
 const Comment = require('../../models/Comment');
 
+/*
 async function getComments(req, res) {
   const commentData = await Comment.findAll().catch((err) => {
     res.json(err);
@@ -14,19 +15,33 @@ async function getComments(req, res) {
 
 let allComments = [];
 getComments();
+*/
 
 
 // GET route to retrieve images from Cloudinary
 router.get('/:id', async(req, res) => {
   // findOne method
+  const commentData = await Comment.findAll({
+    where: {
+        image_id: req.params.id
+    },
+    attributes: ['id', 'text', 'image_id', 'createdAt']
+  })
   const recipeData = await CraftingRecipe.findOne({
       where: {
           id: req.params.id
       },
       attributes: ['id', 'keyword', 'imageUrl']
-  })
+  });
+  let matchingComments = [];
+  if(!commentData) {
+    matchingComments == "";
+  }
+  else {
+    matchingComments = commentData.map((comment) => comment.get({ plain: true }));
+  }
   const individualRecipe = recipeData.get({ plain: true });
-  res.render('images', { individualRecipe, allComments });
+  res.render('images', { matchingComments, individualRecipe });
 });
 
 module.exports = router;
