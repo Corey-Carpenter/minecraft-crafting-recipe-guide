5be513e58f60d2765bc9b0240ff63dbc09b57852
Michael O'Sullivan <michaelhallosullivan@gmail.com>
2023-07-05 21:31:22 -0700

    fixed merge conflict

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');;
 const apiRoutes = require('./api');
-//const craftingRecipeRoutes = require('./craftingRecipeRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
